5f13405b69ffa3b112cf4a54e7b037bfd90e3656
Philippe Girard <philippe.girard5@gmail.com>
2017-10-26 15:12:34 -0400

    finish frond end

--- server/ejs_routes.js
+++ server/ejs_routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // index page 
 router.get('/', function(req, res) {
-    res.render('index');
+    res.render('login');
 });
 
 // login page 
@@ -17,6 +17,23 @@ router.post('/student', function(req, res) {
 	var user_id = req.body.id
     res.render('student/transactions', {
     	user_id: user_id
+    });
+});
+
+// merchant products page 
+router.get('/merchant/products', function(req, res) { // TODO change to a post
+	var merchant_id = req.body.id
+    res.render('merchant/products', {
+    	merchant_id: merchant_id
+    });
+});
+
+
+// merchant sales page 
+router.get('/merchant/sales', function(req, res) { // TODO change to a post
+	var merchant_id = req.body.id
+    res.render('merchant/sales', {
+    	merchant_id: merchant_id
     });
 });
 
