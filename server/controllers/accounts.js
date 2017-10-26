3ce78ec20fb498aafeffc337af0d6130c300ebad
Donavan Martin <donavanmartin@MacBook-Pro-de-Donavan.local>
2017-10-26 14:48:24 -0400

    Check already existing user when create.

--- server/controllers/accounts.js
+++ server/controllers/accounts.js
@@ -1,23 +1,33 @@
 var Accounts = require('../models').Accounts
 var InvalidAccount = require('../exceptions/invalidAccount')
+var AlreadyExist = require('../exceptions/alreadyExist')
 var verify = require('../helpers/parameters');
 
 module.exports = {
   create: function(req, res, next) {
     verify.verifyParameter(req.body.name, 'name');
     verify.verifyAccountTypeParameter(req.body.type, 'type');
-
-    return Accounts
-      .create({
-        name: req.body.name,
-        type: req.body.type,
-        amount: req.body.amount || 0.00,
-        card: req.body.card || null
-      })
-      .then(account => res.status(201).json(account))
-      .catch(function (err) {
-        next(err);
-      });
+ 
+    Accounts.count({ where: {name: req.body.name} })
+      .then(count => {
+        if (count != 0) {
+          throw new AlreadyExist('Username:'+req.body.name+' alreay exist');
+        }else{
+         return Accounts
+             .create({
+             name: req.body.name,
+             type: req.body.type,
+             amount: req.body.amount || 0.00,
+             card: req.body.card || null
+          })
+          .then(account => res.status(201).json(account))
+          .catch(function (err) {
+            next(err);
+          });
+        }
+      }).catch(function (err) {
+      next(err);
+    });
   },
 
   list: function(req, res, next) {
