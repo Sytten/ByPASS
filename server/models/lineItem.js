ee7b510bd2fb1ebd7f1023bf0d567c878b9068e1
Philippe Girard <philippe.girard5@gmail.com>
2017-11-03 22:30:07 -0400

    multiple items creation

--- server/models/lineItem.js
+++ server/models/lineItem.js
@@ -13,7 +13,6 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        LineItem.hasOne(models.Item)
       }
     }
   });
