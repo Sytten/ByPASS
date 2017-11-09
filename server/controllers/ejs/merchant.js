3271740c9cdce4438680c9a40ebef69d53c5d6ee
Donavan_Martin <DonavanMartin@users.noreply.github.com>
2017-11-09 09:24:49 -0500

    Update merchant.js
    indent tab to --> 2 spaces

--- server/controllers/ejs/merchant.js
+++ server/controllers/ejs/merchant.js
@@ -1,23 +1,16 @@
-// models
 var Account = require('../../models').Account
 var Item = require('../../models').Item
-
-// helpers
+var dateFormat = require('dateformat');
 var transaction_helper = require('../../helpers/transaction_helper')
-
-// exceptions
 var InvalidTransaction = require('../../exceptions/invalidTransaction')
 
-// extensions
-var dateFormat = require('dateformat');
-
 module.exports = {
-	products: function(req, res, next) {
-		var merchant_id = req.body.id
-	    res.render('merchant/products', {
-	       merchant_id: merchant_id
-	    });
-	},
+  products: function(req, res, next) {
+    var merchant_id = req.body.id
+    res.render('merchant/products', {
+       merchant_id: merchant_id
+    });
+  },
 
   products_table: function(req, res, next) {
     Item.findAll({where : { merchant: req.body.id }, raw: true}).then(items => {
@@ -25,42 +18,41 @@ module.exports = {
     })
   },
 
-	sales: function(req, res, next) {
-		var merchant_id = req.body.id
-	    res.render('merchant/sales', {
-	       merchant_id: merchant_id
-	    });
-	},
+  sales: function(req, res, next) {
+    var merchant_id = req.body.id
+    res.render('merchant/sales', {
+       merchant_id: merchant_id
+    });
+  },
 
-	sales_transactions: function(req, res, next) {
+  sales_transactions: function(req, res, next) {
     transaction_helper.flatTransactionsById(req.body.id).then(transactions => {
-          // format transactions
-          var formatted_transactions = transactions.map(function (transaction) {
-              return {
-                  article: transaction['lineItems.item.name'],
-                  units: transaction['lineItems.quantity'],
-                  somme: transaction['lineItems.quantity'] * transaction['lineItems.item.price'],
-                  client: transaction['client_t.name'],
-                  date: dateFormat(transaction.createdAt, "yyyy/mm/dd"),
-              };
-          });
+      // format transactions
+      var formatted_transactions = transactions.map(function (transaction) {
+        return {
+          article: transaction['lineItems.item.name'],
+          units: transaction['lineItems.quantity'],
+          somme: transaction['lineItems.quantity'] * transaction['lineItems.item.price'],
+          client: transaction['client_t.name'],
+          date: dateFormat(transaction.createdAt, "yyyy/mm/dd"),
+        };
+      });
 
-          // render transaction table of a merchant
-          res.render('merchant/parts/sales_table', {
-            transactions: formatted_transactions
-          });
+      // render transaction table of a merchant
+      res.render('merchant/parts/sales_table', {
+        transactions: formatted_transactions
+      });
+    })
+    .catch((error) => next(new InvalidTransaction(req.body.id, error))); 
+  },
+
+  sales_total: function(req, res, next) {
+    transaction_helper.flatTransactionsById(req.body.id).then(transactions => {
+      // compute sum of transactions
+      var sum = transactions.reduce((current_sum, transaction) => current_sum + transaction['lineItems.quantity'] * transaction['lineItems.item.price'], 0);
+
+      res.status(201).json({total: sum});
     })
     .catch((error) => next(new InvalidTransaction(req.body.id, error)));
-		
-	},
-
-	sales_total: function(req, res, next) {
-    transaction_helper.flatTransactionsById(req.body.id).then(transactions => {
-          // compute sum of transactions
-          var sum = transactions.reduce((current_sum, transaction) => current_sum + transaction['lineItems.quantity'] * transaction['lineItems.item.price'], 0);
-
-          res.status(201).json({total: sum});
-    })
-    .catch((error) => next(new InvalidTransaction(req.body.id, error)));
-	},
+  },
 }
