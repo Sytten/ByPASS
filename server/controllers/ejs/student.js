5f43038836198270b96ab06a4e4e38e79c2b5c6b
Donavan_Martin <DonavanMartin@users.noreply.github.com>
2017-11-09 09:26:26 -0500

    Update student.js
    correction indent space to 2 spaces

--- server/controllers/ejs/student.js
+++ server/controllers/ejs/student.js
@@ -1,45 +1,37 @@
-// models
 var Account = require('../../models').Account
-
-// helpers
+var dateFormat = require('dateformat');
 var transaction_helper = require('../../helpers/transaction_helper')
-
-// exceptions
 var InvalidTransaction = require('../../exceptions/invalidTransaction')
 
-// extensions
-var dateFormat = require('dateformat');
-
-
 module.exports = {
-	transactions: function(req, res, next) {
-	    res.render('student/transactions');
-	},
+  transactions: function(req, res, next) {
+    res.render('student/transactions');
+  },
 
-	transactions_table: function(req, res, next) {
+  transactions_table: function(req, res, next) {
     transaction_helper.flatTransactionsById(req.body.id).then(transactions => {
-          // format transactions
-          var formatted_transactions = transactions.map(function (transaction) {
-              return {
-                  article: transaction['lineItems.item.name'],
-                  montant: transaction['lineItems.quantity'] * transaction['lineItems.item.price'],
-                  destinataire: transaction['marchand.name'],
-                  date: dateFormat(transaction.createdAt, "yyyy/mm/dd"),
-              };
-          });
+      // format transactions
+      var formatted_transactions = transactions.map(function (transaction) {
+        return {
+          article: transaction['lineItems.item.name'],
+          montant: transaction['lineItems.quantity'] * transaction['lineItems.item.price'],
+          destinataire: transaction['marchand.name'],
+          date: dateFormat(transaction.createdAt, "yyyy/mm/dd"),
+        };
+      });
 
-          // render transaction table of a student
-    		  res.render('student/parts/transactions_table', {
-				    transactions: formatted_transactions
-			    });
+      // render transaction table of a student
+      res.render('student/parts/transactions_table', {
+        transactions: formatted_transactions
+      });
     })
     .catch((error) => next(new InvalidTransaction(req.body.id, error)));
-	},
+  },
 
-	total: function(req, res, next) {
-		Account.findById(req.body.id, {rejectOnEmpty: true}).then(account => {
-			res.status(201).json({total: account.amount});
-		})
-		.catch(() => next(new InvalidAccount(req.body.id)));
-	},
+  total: function(req, res, next) {
+    Account.findById(req.body.id, {rejectOnEmpty: true}).then(account => {
+      res.status(201).json({total: account.amount});
+    })
+    .catch(() => next(new InvalidAccount(req.body.id)));
+  },
 }
