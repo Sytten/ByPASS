fb109b58c947c16910342bae897783d953382df9
Emile Fugulin <emilefugulin@hotmail.com>
2017-11-19 22:20:19 +0000

    Change seeders

--- server/seeders/accounts.js
+++ server/seeders/accounts.js
@@ -17,6 +17,14 @@ module.exports = {
         },
 
         {
+          id: "aaaa20bd-a308-41ac-870a-e2e7b44a59c9",
+          name: "fuge2701",
+          amount: 1200,
+          type: "CUSTOMER",
+          card: "1919af35"
+        },
+
+        {
           id: "1a2b20bd-a308-41ac-870a-e2e7b44a59c9",
           name: "merc2020",
           amount: 0,
